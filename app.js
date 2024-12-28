4dc51209667500b851e629af719d23953eca70d8
Guillaume Savary <savaryguillaume@proton.me>
2024-12-28 19:54:41 +0100

    Optimisation des requêtes, mise en cache API et HTTP, compression

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
-
 const cors = require('cors');
-
+const compression = require('compression');
+const redis = require('redis');
 const notionRoutes = require('./routes/notion');
 
 const app = express();
+const redisClient = redis.createClient();
 
 // Middleware
 app.use(express.json()); // Pour parser le JSON envoyé dans les requêtes
 app.use(cors()); // Permettre les requêtes CORS
+app.use(compression()); // Compression des réponses
+
+// Mise en cache HTTP
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=3600'); // Cache for 1 hour
+    next();
+});
 
 // Routes
-app.use('/api/notion', notionRoutes);
+app.use('/api/notion', (req, res, next) => {
+    req.redisClient = redisClient;
+    next();
+}, notionRoutes);
 
 module.exports = app;
